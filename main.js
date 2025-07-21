a50951aa23138c399cc0c88a575a4d0bf013a100
Rehan Sayyed <srehanm78@gmail.com>
2025-07-22 00:38:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,123 @@
+//Question.1
+let a=7;
+b=9;
+c=a+b;
+console.log("a+b:" + c);
+
+//Question.2
+let x=17;
+y=3;
+console.log(x%y);
+
+//Question.3
+let p=2;
+q=3;
+r=4;
+console.log(p * q * r);
+
+//Question.4
+var sub=250-100;
+console.log(sub);
+res=sub/5;9;
+console.log(res);
+
+//Question.5
+let g=5;
+f=++g;
+console.log(g,f);
+
+//Question.6
+let m=20;
+m+=10;
+console.log(m);
+
+//Question.7
+let n=7;
+n*=7;
+console.log(n);
+
+//Question.8
+let h=10;
+h-=3;
+console.log(h);
+
+//Question.9
+let k=9;
+l="9";
+console.log(k==l);
+console.log(l===k);
+
+//Question.10
+console.log(25>15);
+
+//Question.11
+console.log(1<1.1);
+
+//Question.12
+console.log(5==="5");
+
+//Question.13
+console.log(9<=10 && 49<50);
+console.log(9<=10 && 59<50);
+console.log(10.1<=10 && 49<50);
+
+//Question.14
+console.log(1<10 || 999>100);
+
+//Question.15
+let v= true;
+console.log(v);
+console.log(typeof (v));
+u=false;
+console.log(u && v);
+
+//Question.16
+console.log(!u);
+
+//Question.17
+let z=9;
+if
+(z%2==0){
+console.log(z + " " + "is even");
+}
+else
+    {
+      
+console.log(z + " " +"is odd" );
+    }
+
+    //Question.18
+   for(age=17; age<=18; age++ )
+    if
+    (age>=18)
+    {
+        console.log(age + " " +"adult");
+    }
+else
+{
+    console.log(age + " " + "Not adult")
+}
+
+//Question.19
+for (number=76; number<=77;number++)
+    if(number<77)
+    {console.log(number + " " + "number is smaller");
+
+    }
+    else{
+        console.log(number + " " + "number s greater");
+    }
+    //Question.20
+    var j=5;
+    k=j++ + ++j;
+    console.log(k);
+
+
+
+
+
+
+
+
+
+
